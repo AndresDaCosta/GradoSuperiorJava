c09a31f38ceb52594680f6b1a4811fec52d88167
Andres Da Costa <andrescostaribeiro16@gmail.com>
2018-11-23 03:17:22 +0100

    cadena al reves

--- src/Ilerna/utiles/UtilesArrays.java
+++ src/Ilerna/utiles/UtilesArrays.java
@@ -5,28 +5,30 @@ import java.util.Random;
 public class UtilesArrays {
 
 	
-	public void main (String[] args) {
+	public static void main (String[] args) {
 		
 		
 		int[] array = alearray(6,10,20);
 		
-		for(int i = 0; i <= array.length; i ++) {
+		for(int i = 0; i < array.length; i ++) {
 			
 			System.out.println(array[i]);
 			
 		}
+		
+		System.out.println(alreves("ola"));
 		
 		
 	}
 	
 	
 	
-	public int[] alearray(int end, int Inferior, int Superior){
+	public static int[] alearray(int end, int Inferior, int Superior){
 		
 		Random aleatorio = new Random();
 		int[] array = new int[end];
 		
-		for(int i = 0; i <= end; i ++) {
+		for(int i = 0; i < end; i ++) {
 			
 			array[i] = Inferior + aleatorio.nextInt(Superior - Inferior) ;
 			
@@ -37,6 +39,25 @@ public class UtilesArrays {
 		return array;
 		
 		
+		
+	}
+	public static String alreves(String into) {
+		
+		String out = new String();
+		
+		
+		
+		
+		for(int i = (into.length()-1); i >= 0; i --) {
+			
+			out += into.charAt(i);
+			
+			
+		}
+		
+		
+		
+		return out;
 		
 	}
 	
